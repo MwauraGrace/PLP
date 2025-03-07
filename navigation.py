9fde4f5abc9588cde3ea16637803b69e429e8348
MwauraGrace <wanjikuwainaina.com>
2025-03-07 21:46:20 +0300

    First commit

---
+++ navigation.py
@@ -0,0 +1,10 @@
+print("Hello word!")
+marks = int(input("Enter the student's marks:"))
+if marks >= 70:
+    print("Grade: Distinction")
+elif marks >= 60:
+    print("Grade:Good")
+elif marks >= 50:
+    print("Grade:Pass")
+else :
+    print("Grade: Fail")
